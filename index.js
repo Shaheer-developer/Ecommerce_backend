0dac1e4460dc2a38e698252cc83352bc90eb4ff3
Shaheer-developer <m.shaheer062006@gmail.com>
2024-12-24 01:29:35 +0500

    added some endpoints to add product , delete product and to get all products

--- index.js
+++ index.js
@@ -90,8 +90,18 @@ const ProductSchema = new mongoose.Schema({
 const Product = mongoose.model('Product', ProductSchema);
 
 app.post('/add-product', async (req, res) => {
+    let products = await Product.find({});
+    let id;
+    if(products.length > 0){
+        let last_product_array = products.slice(-1);   
+        let last_product = last_product_array[0];
+        id = last_product.id + 1;
+    }
+    else{
+        id = 1;
+    }
     const product = new Product({
-        id: req.body.id,
+        id: id,
         name: req.body.name,
         image: req.body.image,
         category: req.body.category,
@@ -104,6 +114,21 @@ app.post('/add-product', async (req, res) => {
     res.json({success: true, name: req.body.name});
 });
 
+// creating endpoint for deleting Product
+app.post('/delete-product', async (req, res) => {
+    await Product.findOneAndDelete({id: req.body.id});
+    console.log('Product deleted successfully');
+    res.json({success: true , name: req.body.name});
+});
+
+// creating endpoint for getting all Products
+app.get('/allproducts', async (req, res) => {
+    let products = await Product.find({});
+    console.log("All products fetched",products);
+    res.send(products);
+});
+
+   
 app.listen(process.env.PORT, (error) => {
 if(!error){
     console.log(`Server is running on port ${process.env.PORT}`);
